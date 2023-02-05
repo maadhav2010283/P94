ef435ab22e27f6eddfbc54ad6a506d92e932c124
maadhav2010283 <94798327+maadhav2010283@users.noreply.github.com>
2023-02-05 13:48:58 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,17 @@
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDxQNs98DvI1ZReY1IXrNC7MJazZ4LXgAc",
+    authDomain: "project44-f35d1.firebaseapp.com",
+    databaseURL: "https://project44-f35d1-default-rtdb.firebaseio.com",
+    projectId: "project44-f35d1",
+    storageBucket: "project44-f35d1.appspot.com",
+    messagingSenderId: "129960493560",
+    appId: "1:129960493560:web:f8274ec2a098508705960d"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
